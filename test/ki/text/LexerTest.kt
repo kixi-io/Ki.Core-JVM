84cb5f7962aae6bb1c40015f3b5f9c0c2cf32b68
Daniel Leuck <dan@ikayzo.com>
2020-06-11 14:59:30 +0900

    Removed notEq test

--- test/ki/text/LexerTest.kt
+++ test/ki/text/LexerTest.kt
@@ -15,7 +15,7 @@ class LexerTest : Test() {
         eq("peekChar()", 'H', lex.peekChar())
 
         // System.err.println("testing fail behavior")
-        notEq("peekChar()", 'H', lex.peekChar())
+        // notEq("peekChar()", 'H', lex.peekChar())
         eq("peekChars(3)", "Hel", lex.peekChars(3))
         eq("peekChars(6) - safe peek past the end", "Hello", lex.peekChars(6))
     }
