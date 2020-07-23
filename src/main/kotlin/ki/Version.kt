03dcc2d2ab3557ebe76553de943aff9ff3bfd53b
Daniel Leuck <dan@ikayzo.com>
2020-07-23 17:41:45 +0900

    Testing source control issue in project

--- src/main/kotlin/ki/Version.kt
+++ src/main/kotlin/ki/Version.kt
@@ -12,7 +12,7 @@ import main.kotlin.ki.text.isKiIdentifier
  *  3. Micro version: A positive integer
  *  4. Qualifier: A string such as "alpha" or "beta", allows alphanum, '_' and '-'
  *
- *  // TODO: Change this to match Mavin versions.
+ *  // TODO: Line this up with Maven Verions
  */
 data class Version (var major: Int, var minor: Int = 0, var micro: Int = 0, var qualifier: String = "") :
     Comparable<Any?> {
