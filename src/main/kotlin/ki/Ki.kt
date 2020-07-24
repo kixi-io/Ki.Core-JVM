1df5da6faf0d668f7d2599520e280e22e9fae114
Daniel Leuck <dan@ikayzo.com>
2020-07-24 12:39:04 +0900

    Added Java-friendly annotations

--- src/main/kotlin/ki/Ki.kt
+++ src/main/kotlin/ki/Ki.kt
@@ -21,19 +21,19 @@ class Ki {
         const val DATE_FORMAT = "yyyy/MM/dd"
 
         // TODO: Separate patterns for parsing and formatting (formatting should use padding as appropriate)
-        val LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("y/M/d")
-        val LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("y/M/d@H:m:s.nnnnnnnnn")
-        val ZONED_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("y/M/d@H:m:s.nnnnnnnnn/VV")
+        @JvmField val LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("y/M/d")
+        @JvmField val LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("y/M/d@H:m:s.nnnnnnnnn")
+        @JvmField val ZONED_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("y/M/d@H:m:s.nnnnnnnnnVV")
 
         /**
          * The Ki standard DATE_TIME format yyyy/MM/dd-HH:mm:ss.SSS-z
          *
          * Note: Ki uses a 24 hour clock (0-23)
          */
-        const val DATE_TIME_FORMAT = DATE_FORMAT + "-" + TIME_FORMAT
+        const val DATE_TIME_FORMAT = DATE_FORMAT + "@" + TIME_FORMAT
 
         // TODO support all Ki types
-        fun format(obj: Any?): String {
+        @JvmStatic fun format(obj: Any?): String {
             return when (obj) {
                 null -> "nil"
                 is String -> "\"$obj\""
