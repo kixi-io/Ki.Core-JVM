00b2cd0c6f89b5dc536a84f133aabaa227274333
Daniel Leuck <dan@ikayzo.com>
2020-09-01 16:13:55 +0900

    Moved kclass from TypeDef to Type

--- src/main/kotlin/io/kixi/Type.kt
+++ src/main/kotlin/io/kixi/Type.kt
@@ -1,24 +1,32 @@
 package io.kixi
 
-enum class Type(supertype: Type?) {
+import java.math.BigDecimal
+import kotlin.reflect.KClass
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.time.Duration
+
+enum class Type(kclass: KClass<*>, supertype: Type?) {
     // Super types
-    Any(null),
-    Number(Any),
+    Any(kotlin.Any::class, null),
+    Number(kotlin.Number::class, Any),
 
     // Base types
-    String(Any), Char(Any),
-    Int(Number), Long(Number),
-    Float(Number), Double(Number), Decimal(Number),
-    Bool(Any),
-    URL(Any),
-    Date(Any), LocalDateTime(Any), ZonedDateTime(Any),
-    Duration(Any),
-    Version(Any),
-    Blob(Any),
-    Quantity(Any),
-    Range(Any),
-    List(Any), Map(Any),
+    String(kotlin.String::class, Any), Char(kotlin.Char::class, Any),
+    Int(kotlin.Int::class, Number), Long(kotlin.Long::class, Number),
+    Float(kotlin.Float::class, Number), Double(kotlin.Double::class, Number), Decimal(BigDecimal::class, Number),
+    Bool(kotlin.Boolean::class, Any),
+    URL(java.net.URL::class, Any),
+    Date(java.time.LocalDate::class, Any), LocalDateTime(java.time.LocalDateTime::class, Any),
+        ZonedDateTime(java.time.ZonedDateTime::class, Any),
+    Duration(java.time.Duration::class, Any),
+    Version(io.kixi.Version::class, Any),
+    Blob(ByteArray::class, Any),
+    Quantity(io.kixi.uom.Quantity::class, Any),
+    Range(io.kixi.Range::class, Any),
+    List(java.util.List::class, Any), Map(java.util.Map::class, Any),
 
     // nil
-    nil(null);
+    nil(KClass::class,null);
 }
--- src/main/kotlin/io/kixi/TypeDef.kt
+++ src/main/kotlin/io/kixi/TypeDef.kt
@@ -10,7 +10,7 @@ import java.time.ZonedDateTime
 import java.time.Duration
 import kotlin.reflect.KClass
 
-open class TypeDef(val type:Type, val nullable:Boolean, val kclass:KClass<*>) {
+open class TypeDef(val type:Type, val nullable:Boolean) {
 
     override fun toString(): String {
         val nullChar = if (nullable) "?" else ""
@@ -22,60 +22,60 @@ open class TypeDef(val type:Type, val nullable:Boolean, val kclass:KClass<*>) {
      */
     companion object {
         // Super types
-        val Any = TypeDef(Type.Any, false, Any::class)
-        val Any_N= TypeDef(Type.Any, true, Any::class)
+        val Any = TypeDef(Type.Any, false)
+        val Any_N= TypeDef(Type.Any, true)
 
-        val Number= TypeDef(Type.Number, false, Number::class)
-        val Number_N = TypeDef(Type.Number, true, Number::class)
+        val Number= TypeDef(Type.Number, false)
+        val Number_N = TypeDef(Type.Number, true)
 
         // Base types
-        val String = TypeDef(Type.String, false, String::class)
-        val String_N = TypeDef(Type.String, true, String::class)
+        val String = TypeDef(Type.String, false)
+        val String_N = TypeDef(Type.String, true)
 
-        val Char = TypeDef(Type.Char, false, Char::class)
-        val Char_N = TypeDef(Type.Char, true, Char::class)
+        val Char = TypeDef(Type.Char, false)
+        val Char_N = TypeDef(Type.Char, true)
 
-        val Int = TypeDef(Type.Int, false, Int::class)
-        val Int_N= TypeDef(Type.Int, true, Int::class)
+        val Int = TypeDef(Type.Int, false)
+        val Int_N= TypeDef(Type.Int, true)
 
-        val Long = TypeDef(Type.Long, false, Long::class)
-        val Long_N = TypeDef(Type.Long, true, Long::class)
+        val Long = TypeDef(Type.Long, false)
+        val Long_N = TypeDef(Type.Long, true)
 
-        val Float = TypeDef(Type.Float, false, Float::class)
-        val Float_N = TypeDef(Type.Float, true, Float::class)
+        val Float = TypeDef(Type.Float, false)
+        val Float_N = TypeDef(Type.Float, true)
 
-        val Double = TypeDef(Type.Double, false, Double::class)
-        val Double_N = TypeDef(Type.Double, true, Double::class)
+        val Double = TypeDef(Type.Double, false)
+        val Double_N = TypeDef(Type.Double, true)
 
-        val Decimal = TypeDef(Type.Decimal, false, BigDecimal::class)
-        val Decimal_N = TypeDef(Type.Decimal, true, BigDecimal::class)
+        val Decimal = TypeDef(Type.Decimal, false)
+        val Decimal_N = TypeDef(Type.Decimal, true)
 
-        val Bool = TypeDef(Type.Bool, false, Boolean::class)
-        val Bool_N = TypeDef(Type.Bool, true, Boolean::class)
+        val Bool = TypeDef(Type.Bool, false)
+        val Bool_N = TypeDef(Type.Bool, true)
 
-        val URL = TypeDef(Type.URL, false, URL::class)
-        val URL_N = TypeDef(Type.URL, true, URL::class)
+        val URL = TypeDef(Type.URL, false)
+        val URL_N = TypeDef(Type.URL, true)
 
-        val Date = TypeDef(Type.Date, false, LocalDate::class)
-        val Date_N = TypeDef(Type.Date, true, LocalDate::class)
+        val Date = TypeDef(Type.Date, false)
+        val Date_N = TypeDef(Type.Date, true)
 
-        val LocalDateTime = TypeDef(Type.LocalDateTime, false, LocalDateTime::class)
-        val LocalDateTime_N = TypeDef(Type.LocalDateTime, true, LocalDateTime::class)
+        val LocalDateTime = TypeDef(Type.LocalDateTime, false)
+        val LocalDateTime_N = TypeDef(Type.LocalDateTime, true)
 
-        val ZonedDateTime = TypeDef(Type.ZonedDateTime, false, ZonedDateTime::class)
-        val ZonedDateTime_N = TypeDef(Type.ZonedDateTime, true, ZonedDateTime::class)
+        val ZonedDateTime = TypeDef(Type.ZonedDateTime, false)
+        val ZonedDateTime_N = TypeDef(Type.ZonedDateTime, true)
 
-        val Duration = TypeDef(Type.Duration, false, Duration::class)
-        val Duration_N = TypeDef(Type.Duration, true, Duration::class)
+        val Duration = TypeDef(Type.Duration, false)
+        val Duration_N = TypeDef(Type.Duration, true)
 
-        val Version = TypeDef(Type.Version, false, Version::class)
-        val Version_N = TypeDef(Type.Version, true, Version::class)
+        val Version = TypeDef(Type.Version, false)
+        val Version_N = TypeDef(Type.Version, true)
 
-        val Blob = TypeDef(Type.Blob, false, ByteArray::class)
-        val Blob_N = TypeDef(Type.Blob, true, ByteArray::class)
+        val Blob = TypeDef(Type.Blob, false)
+        val Blob_N = TypeDef(Type.Blob, true)
 
         // nil
-        val nil = TypeDef(Type.nil, true, KClass::class)
+        val nil = TypeDef(Type.nil, true)
     }
 
     open val generic: Boolean get() = false;
@@ -83,7 +83,7 @@ open class TypeDef(val type:Type, val nullable:Boolean, val kclass:KClass<*>) {
 
 // TODO
 class QuantityDef(nullable:Boolean, val unit:Unit, val numType:Type) :
-    TypeDef(Type.Quantity, nullable, Quantity::class) {
+    TypeDef(Type.Quantity, nullable) {
 
     val nullChar = if (nullable) "?" else ""
     val numTypeSuffix = when(numType) {
@@ -107,20 +107,20 @@ class QuantityDef(nullable:Boolean, val unit:Unit, val numType:Type) :
     override val generic: Boolean get() = true;
 }
 
-class RangeDef(nullable:Boolean, val valueDef: TypeDef) : TypeDef(Type.Range, nullable, Range::class) {
+class RangeDef(nullable:Boolean, val valueDef: TypeDef) : TypeDef(Type.Range, nullable) {
     val nullChar = if (nullable) "?" else ""
     override fun toString() = "$type<$valueDef>$nullChar"
     override val generic: Boolean get() = true;
 }
 
-class ListDef(nullable:Boolean, val valueDef: TypeDef) : TypeDef(Type.List, nullable, List::class) {
+class ListDef(nullable:Boolean, val valueDef: TypeDef) : TypeDef(Type.List, nullable) {
     val nullChar = if (nullable) "?" else ""
     override fun toString() = "$type<$valueDef>$nullChar"
     override val generic: Boolean get() = true;
 }
 
 class MapDef(nullable:Boolean, val keyTypeDef: TypeDef, val valueTypeDef: TypeDef) :
-    TypeDef(Type.Map, nullable, Map::class) {
+    TypeDef(Type.Map, nullable) {
     val nullChar = if (nullable) "?" else ""
     override fun toString() = "$type<$keyTypeDef, $valueTypeDef>$nullChar"
     override val generic: Boolean get() = true;
