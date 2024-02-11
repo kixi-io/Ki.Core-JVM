ffe23b5f308fd263ad18e66c799ca26cfc4df0f1
singingbush <singingbush@hotmail.com>
2024-02-11 10:36:54 +0000

    publish build artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 group = "io.kixi"
 version = "1.0.0-beta-4"
@@ -6,30 +7,140 @@ description = "ki-core"
 
 plugins {
     `java-library`
+    `maven-publish`
     kotlin("jvm") version "1.9.+"
+    id("org.jetbrains.dokka") version "1.9.+"
+    signing
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+        languageVersion.set(KotlinVersion.KOTLIN_1_9)
+    }
+}
 
 repositories { mavenCentral() }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
     testImplementation(platform("org.junit:junit-bom:5.10.+"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        apiVersion = "1.9"
-        languageVersion = "1.9"
-        jvmTarget = "${JavaVersion.VERSION_11}"
-        // allWarningsAsErrors = true
+// Ensure that artifacts for jar, kotlinSourcesJar, and dokkaJavadocJar
+// tasks all start with same text (should be lowercase)
+tasks.withType<org.gradle.jvm.tasks.Jar>() {
+    archiveBaseName.set("ki-core")
+}
+
+// use Dokka for generating a javadoc jar
+tasks.register<Jar>("dokkaHtmlJar") {
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("html-docs")
+}
+tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+// Allows all artifacts to be built using: "./gradlew buildAll"
+tasks.register("buildAll") {
+    dependsOn(":jar", ":kotlinSourcesJar", ":dokkaJavadocJar")
+}
+
+// todo: publishing to maven central will require signed artifacts
+//signing {
+//    sign(publishing.publications["mavenJava"])
+//}
+
+val sourcesJar by tasks.named("kotlinSourcesJar")
+val javadocJar by tasks.named("dokkaJavadocJar")
+
+publishing {
+    repositories {
+        maven {
+            if (System.getenv().containsKey("CI")) {
+                // todo: Rather than use GitHub Packages it would be better to publish to maven central (requires signed artifacts)
+                name = "GitHub-Packages"
+                url = uri("https://maven.pkg.github.com/kixi-io/Ki.Core-JVM/")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            } else {
+                val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
+                val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
+                val localReleasePath = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                url = uri(localReleasePath)
+                name = "local-build-dir"
+            }
+        }
+    }
+    publications {
+        // This section configures all the required fields that are needed when releasing to a maven repo
+        // see: https://docs.gradle.org/current/userguide/publishing_maven.html
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            pom {
+                groupId = "${project.group}"
+                artifactId = "ki-core"
+                name = "Ki.Core"
+                description = "A JVM implementation of the Ki.Core library"
+                url = "http://kixi.io"
+                properties = mapOf(
+                    "project.build.sourceEncoding" to "UTF-8",
+                    "project.reporting.outputEncoding" to "UTF-8",
+                    "maven.compiler.source" to JavaVersion.VERSION_11.majorVersion
+                )
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://raw.githubusercontent.com/kixi-io/Ki.Core-JVM/master/LICENSE"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "dleuck"
+                        name = "Daniel Leuck"
+                    }
+                    developer {
+                        id = "singingbush"
+                        name = "Samael"
+                    }
+                    developer {
+                        id = "alessiostalla"
+                        name = "Alessio Stalla"
+                    }
+                }
+                issueManagement {
+                    system = "Pivotal-Tracker"
+                    url = "https://www.pivotaltracker.com/n/projects/2462351"
+                }
+                scm {
+                    connection = "scm:git:git://github.com:kixi-io/Ki.Core-JVM.git"
+                    developerConnection = "scm:git:ssh://github.com:kixi-io/Ki.Core-JVM.git"
+                    url = "http://github.com/kixi-io/Ki.Core-JVM/"
+                }
+            }
+        }
     }
 }
 
